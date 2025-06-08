8973fef8c702005f4b3408f864fff4ad6625b20b
storm coenradie <stormcoenradie@storms-MacBook-Air.local>
2025-06-08 11:44:45 +0200

    Auto update

--- autopush.js
+++ autopush.js
@@ -1,13 +1,20 @@
-// auto-push.js
 const chokidar = require('chokidar');
 const { exec } = require('child_process');
 
-chokidar.watch('./index.html').on('change', () => {
+const watcher = chokidar.watch('./', {
+  ignored: /(^|[\/\\])(\.git|node_modules)/, // ignore .git and node_modules folders
+  persistent: true,
+});
+
+watcher.on('change', (path) => {
+  console.log(`${path} changed, pushing updates...`);
+  
   exec('git add . && git commit -m "Auto update" && git push', (err, stdout, stderr) => {
     if (err) {
-      console.error('Error:', err);
-    } else {
-      console.log('Site updated:', stdout);
+      console.error('Error pushing changes:', err.message);
+      if (stderr) console.error(stderr);
+      return;
     }
+    console.log('Site updated successfully:\n', stdout);
   });
 });
